b1d56009c101fd90c758dab91112554bc865fecd
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-20 20:14:43 -0700

    Removed unnecessary test codes below module exports line.

--- wordsearch.js
+++ wordsearch.js
@@ -52,42 +52,4 @@ const wordSearch = (letters, word) => {
   return false;
 };
 
-
-
 module.exports = wordSearch;
-
-const wordSearch2 = function(arr, word) {
-   for (let i = 0; i < arr.length; i++) {
-     let emptyArr = [];
-
-     for (let j = 0; j >= 0; j--) {
-         console.log(arr[i][j])
-         emptyArr.push(arr[i][j]);
-     }
-     console.log(emptyArr);     
-   }
-}
-
-const result = wordSearch2(
-  [
-    ["A", "B", "C", "D"],
-    ["E", "F", "G", "H"],
-    ["I", "J", "K", "L"]
-  ],
-  "ARIELLE"
-);
-
-/*
-arr rows = 3, i range [0, 1, 2]
-arr cols = 4, j range [0, 1, 2, 3]
-
-                        arr[0][0]   1 element
-            arr[1][0],  arr[0][1]   2
-arr[2][0],  arr[1][1],  arr[0][2]   3
-arr[2][1],  arr[1][2],  arr[0][3]   3
-arr[2][2],  arr[1][1]               2
-arr[2][3]                           1
-
-*/
-
-console.log(result);
