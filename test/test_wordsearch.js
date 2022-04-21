8158bf280865e8a00cdee4b9ed18960a2cf6d6d9
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-20 20:05:47 -0700

    Pair programming with Yoyo Yang (GitHub: @yoyotruly). Implemented tests for vertical traversal - true if word found and separate false test if word not found. Added tests to take care of if the word spans multiple lines horizontally as well as separate test for if word spans multiple lines vertically. Added test to check for if the word matrix is an empty array, return false. Added separate test to check and return false if data type for first argument fed in is not an array.

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,131 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return false if the word spans multple lines vertically", function () {
+    const result = wordSearch(
+      [
+        ["A", "H", "C", "F", "Q", "U", "A", "L"],
+        ["S", "T", "I", "N", "F", "E", "L", "D"],
+        ["E", "O", "S", "F", "Q", "U", "A", "L"],
+        ["H", "N", "J", "T", "E", "V", "R", "G"],
+        ["W", "H", "C", "S", "Y", "E", "R", "L"],
+        ["B", "F", "R", "E", "N", "E", "Y", "B"],
+        ["R", "B", "T", "W", "A", "P", "A", "I"],
+        ["I", "D", "C", "A", "K", "U", "A", "S"],
+        ["G", "Z", "K", "F", "Q", "U", "A", "L"],
+      ],
+       "BRIGHTON"
+     );
+
+    assert.isFalse(result);
+  });
+
+
+  it("should return true if the word is present vertically", function () {
+    const result = wordSearch(
+      [
+        ["A", "W", "C", "F", "Q", "U", "A", "L"],
+        ["S", "E", "I", "N", "F", "E", "L", "D"],
+        ["Y", "F", "C", "F", "Q", "U", "A", "L"],
+        ["H", "M", "J", "T", "E", "V", "R", "G"],
+        ["W", "H", "C", "S", "Y", "E", "R", "L"],
+        ["B", "F", "R", "E", "N", "E", "Y", "B"],
+        ["U", "B", "T", "W", "A", "P", "A", "I"],
+        ["O", "D", "C", "A", "K", "U", "A", "S"],
+        ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+      ],
+      "LARRY"
+    );
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present", function () {
+    const result = wordSearch(
+      [
+        ["A", "W", "C", "F", "Q", "U", "A", "L"],
+        ["S", "E", "I", "N", "F", "E", "L", "D"],
+        ["Y", "F", "C", "F", "Q", "U", "A", "L"],
+        ["H", "M", "J", "T", "E", "V", "R", "G"],
+        ["W", "H", "C", "S", "Y", "E", "R", "L"],
+        ["B", "F", "R", "E", "N", "E", "Y", "B"],
+        ["U", "B", "T", "W", "A", "P", "A", "I"],
+        ["O", "D", "C", "A", "K", "U", "A", "S"],
+        ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+      ],
+      "ROBERT"
+    );
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if the word spans multiple lines horizontally", function () {
+    const result = wordSearch(
+    [
+      ["A", "W", "C", "F", "Q", "U", "A", "L"],
+      ["S", "E", "I", "N", "F", "E", "L", "D"],
+      ["E", "R", "S", "F", "Q", "U", "A", "L"],
+      ["H", "M", "J", "T", "E", "V", "R", "G"],
+      ["W", "H", "C", "S", "Y", "E", "R", "L"],
+      ["B", "F", "R", "E", "N", "E", "Y", "B"],
+      ["U", "B", "T", "W", "A", "P", "A", "I"],
+      ["O", "D", "C", "A", "K", "U", "A", "S"],
+      ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+    ],
+    "FELDERS");
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if word matrix is empty", function () {
+    const result = wordSearch([], "ROBERT");
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if word matrix is not an array", function () {
+    const result = wordSearch("Hello", "ROBERT");
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word appears backwards", function () {
+    const result = wordSearch(
+      [
+        ["A", "W", "C", "F", "Q", "U", "A", "L"],
+        ["S", "E", "I", "N", "F", "E", "L", "D"],
+        ["E", "R", "S", "F", "Q", "U", "A", "L"],
+        ["H", "M", "J", "T", "E", "V", "R", "G"],
+        ["W", "H", "C", "S", "Y", "E", "R", "L"],
+        ["B", "F", "R", "E", "N", "E", "Y", "B"],
+        ["U", "B", "T", "W", "A", "P", "A", "I"],
+        ["O", "D", "C", "A", "K", "U", "A", "S"],
+        ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+      ],
+      "NERF"
+    );
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word appears diagonally", function () {
+    const result = wordSearch(
+      [
+        ["A", "W", "C", "F", "Q", "U", "A", "L"],
+        ["S", "R", "I", "N", "F", "E", "L", "D"],
+        ["E", "R", "I", "F", "Q", "U", "A", "L"],
+        ["H", "M", "J", "E", "E", "V", "R", "G"],
+        ["W", "H", "C", "S", "L", "E", "R", "L"],
+        ["B", "F", "R", "E", "N", "L", "Y", "B"],
+        ["U", "B", "T", "W", "A", "P", "E", "I"],
+        ["O", "D", "C", "A", "K", "U", "A", "S"],
+        ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+      ],
+      "ARIELLE"
+    );
+
+    assert.isTrue(result);
+  });
+
 });
